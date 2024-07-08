220b6e638be9f64f870895cdcf7befe73fb3cdc6
harshyadav02 <harshyadav5736@gmail.com>
2024-07-08 19:19:06 +0530

    added password encrpyter

--- src/main/java/com/harsh/journalApp/service/UserService.java
+++ src/main/java/com/harsh/journalApp/service/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.harsh.journalApp.entity.User;
@@ -15,7 +17,13 @@ public class UserService {
     @Autowired
     public UserEntryRepository userRepository;
 
+    private static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
     public void saveEntry(User user) {
+        userRepository.save(user);
+    }
+
+    public void saveNewUser(User user) {
+        user.setPassword(PASSWORD_ENCODER.encode(user.getPassword()));
         userRepository.save(user);
     }
 
